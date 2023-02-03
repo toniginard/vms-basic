4740831478d9d7058cea4c9e1914599a84be6352
Toni Ginard <toni.ginard@gmail.com>
2023-02-03 11:42:47 +0100

    Upgraded PHP to version 8.2

--- Vagrantfile
+++ Vagrantfile
@@ -1,20 +1,20 @@
 Vagrant.configure("2") do |config|
 
-  config.vm.box = "ubuntu/bionic64"
+  config.vm.box = "ubuntu/jammy64"
 
   config.vm.provision :shell, inline: "hostnamectl set-hostname local"
   config.vm.provision :hosts do |provisioner|
     provisioner.add_host '127.0.0.1', ["server.local.cat"]
   end
 
-  config.vm.network "private_network", ip: "192.168.33.5"
+  config.vm.network "private_network", ip: "192.168.33.20"
 
   config.vm.synced_folder "../web", "/var/www/html"
 
   config.vm.provider "virtualbox" do |v|
     v.customize ["modifyvm", :id, "--memory", "1024"]
     v.customize ["modifyvm", :id, "--cpus", "2"]
-    v.customize ["modifyvm", :id, "--name", "PHP 7.3"]
+    v.customize ["modifyvm", :id, "--name", "PHP 8.2"]
     v.customize ["modifyvm", :id, "--cpuexecutioncap", "100"]
     v.customize ["modifyvm", :id, "--natdnshostresolver1", "on"]
     v.customize ["modifyvm", :id, "--natdnsproxy1", "on"]
--- bootstrap.sh
+++ bootstrap.sh
@@ -15,8 +15,8 @@ sudo apt-get autoremove -y  &> /dev/null
 echo 'Install base packages'
 sudo apt-get install -y ghostscript imagemagick vsftpd openssl &> /dev/null
 
-echo 'Install PHP 7.3'
-sudo apt-get install -y libapache2-mod-php7.3 php7.3-common php7.3-ldap php7.3-zip php7.3-imap php7.3-intl php7.3-mbstring php7.3-mysql php7.3-pgsql php7.3-xml php7.3-gd php7.3-xmlrpc php7.3-curl php7.3-soap php7.3-json php7.3-sqlite3 php-memcache php-imagick php-gettext &> /dev/null
+echo 'Install PHP 8.2'
+sudo apt-get install -y libapache2-mod-php8.2 php8.2-common php8.2-ldap php8.2-zip php8.2-imap php8.2-intl php8.2-mbstring php8.2-mysql php8.2-pgsql php8.2-xml php8.2-gd php8.2-xmlrpc php8.2-curl php8.2-soap php8.2-sqlite3 php-imagick php8.2-gettext &> /dev/null
 
 echo 'Log permissions'
 sudo chmod -R 777 /var/log
@@ -33,26 +33,26 @@ sudo echo "Europe/Madrid" | sudo tee /etc/timezone  &> /dev/null
 sudo dpkg-reconfigure -f noninteractive tzdata &> /dev/null
 
 echo 'PHP Configuration'
-sudo sed -i '$ a\date.timezone = "Europe/Madrid"' "/etc/php/7.3/apache2/php.ini"
-sudo sed -i "s/error_reporting = .*/error_reporting = E_ALL/" /etc/php/7.3/apache2/php.ini
-sudo sed -i "s/memory_limit = .*/memory_limit = 256M/" /etc/php/7.3/apache2/php.ini
-sudo sed -i "s/display_errors = .*/display_errors = On/" /etc/php/7.3/apache2/php.ini
-sudo sed -i "s/display_startup_errors = .*/display_startup_errors = On/" /etc/php/7.3/apache2/php.ini
-sudo sed -i "s/post_max_size = .*/post_max_size = 100M/" /etc/php/7.3/apache2/php.ini
-sudo sed -i "s/upload_max_filesize = .*/upload_max_filesize = 100M/" /etc/php/7.3/apache2/php.ini
-sudo sed -i "s/allow_url_fopen = .*/allow_url_fopen = Off/" /etc/php/7.3/apache2/php.ini
-sudo sed -i "s/;error_log = php_errors.log/error_log = \/var\/log\/apache2\/php_errors.log/" /etc/php/7.3/apache2/php.ini
-sudo sed -i "s/max_execution_time = .*/max_execution_time = 300/" /etc/php/7.3/apache2/php.ini
+sudo sed -i '$ a\date.timezone = "Europe/Madrid"' /etc/php/8.2/apache2/php.ini
+sudo sed -i "s/error_reporting = .*/error_reporting = E_ALL/" /etc/php/8.2/apache2/php.ini
+sudo sed -i "s/memory_limit = .*/memory_limit = 256M/" /etc/php/8.2/apache2/php.ini
+sudo sed -i "s/display_errors = .*/display_errors = On/" /etc/php/8.2/apache2/php.ini
+sudo sed -i "s/display_startup_errors = .*/display_startup_errors = On/" /etc/php/8.2/apache2/php.ini
+sudo sed -i "s/post_max_size = .*/post_max_size = 100M/" /etc/php/8.2/apache2/php.ini
+sudo sed -i "s/upload_max_filesize = .*/upload_max_filesize = 100M/" /etc/php/8.2/apache2/php.ini
+sudo sed -i "s/allow_url_fopen = .*/allow_url_fopen = Off/" /etc/php/8.2/apache2/php.ini
+sudo sed -i "s/;error_log = php_errors.log/error_log = \/var\/log\/apache2\/php_errors.log/" /etc/php/8.2/apache2/php.ini
+sudo sed -i "s/max_execution_time = .*/max_execution_time = 300/" /etc/php/8.2/apache2/php.ini
 
-sudo sed -i '$ a\date.timezone = "Europe/Madrid"' /etc/php/7.3/cli/php.ini
-sudo sed -i "s/error_reporting = .*/error_reporting = E_ALL/" /etc/php/7.3/cli/php.ini
-sudo sed -i "s/memory_limit = .*/memory_limit = 256M/" /etc/php/7.3/cli/php.ini
-sudo sed -i "s/display_errors = .*/display_errors = On/" /etc/php/7.3/cli/php.ini
-sudo sed -i "s/display_startup_errors = .*/display_startup_errors = On/" /etc/php/7.3/cli/php.ini
+sudo sed -i '$ a\date.timezone = "Europe/Madrid"' /etc/php/8.2/cli/php.ini
+sudo sed -i "s/error_reporting = .*/error_reporting = E_ALL/" /etc/php/8.2/cli/php.ini
+sudo sed -i "s/memory_limit = .*/memory_limit = 256M/" /etc/php/8.2/cli/php.ini
+sudo sed -i "s/display_errors = .*/display_errors = On/" /etc/php/8.2/cli/php.ini
+sudo sed -i "s/display_startup_errors = .*/display_startup_errors = On/" /etc/php/8.2/cli/php.ini
 # Next line is commented on cli to allow composer install
-#sudo sed -i "s/allow_url_fopen = .*/allow_url_fopen = Off/" /etc/php/7.3/cli/php.ini
-sudo sed -i "s/;error_log = php_errors.log/error_log = \/var\/log\/apache2\/php_errors.log/" /etc/php/7.3/cli/php.ini
-sudo sed -i "s/max_execution_time = .*/max_execution_time = 300/" /etc/php/7.3/cli/php.ini
+#sudo sed -i "s/allow_url_fopen = .*/allow_url_fopen = Off/" /etc/php/8.2/cli/php.ini
+sudo sed -i "s/;error_log = php_errors.log/error_log = \/var\/log\/apache2\/php_errors.log/" /etc/php/8.2/cli/php.ini
+sudo sed -i "s/max_execution_time = .*/max_execution_time = 300/" /etc/php/8.2/cli/php.ini
 
 sudo sed -i "s/www-data/ubuntu/g" /etc/apache2/envvars
 sudo sed -i "s/www-data/ubuntu/g" /etc/apache2/envvars
@@ -77,13 +77,13 @@ fi
 
 echo 'Configure MySQL'
 export DEBIAN_FRONTEND="noninteractive"
-sudo debconf-set-selections <<< "mysql-server-5.7 mysql-server/root_password password $pass"
-sudo debconf-set-selections <<< "mysql-server-5.7 mysql-server/root_password_again password $pass"
+sudo debconf-set-selections <<< "mysql-server-8.0 mysql-server/root_password password $pass"
+sudo debconf-set-selections <<< "mysql-server-8.0 mysql-server/root_password_again password $pass"
 
 sudo apt-get update &> /dev/null
 
-echo 'Install MySQL server 5.7'
-sudo apt-get install -y mysql-server-5.7 &> /dev/null
+echo 'Install MySQL server 8.0'
+sudo apt-get install -y mysql-server-8.0 &> /dev/null
 
 sudo sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mysql/mysql.conf.d/mysqld.cnf
 sudo sed -i 's/\[mysqld\]/\[mysqld\]\nwait_timeout = 100\nmax_connections=500/g' /etc/mysql/mysql.conf.d/mysqld.cnf
